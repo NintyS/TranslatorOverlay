e87af1d5a77fad5eaf99a43c8ccc3138a46b6384
NintySwinty <31569763+NintyS@users.noreply.github.com>
2022-10-16 12:50:34 +0200

    Działa dzięki: obscurity z 4programmers

--- Assets/Text.cs
+++ Assets/Text.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 using System.Threading;
 using System.Diagnostics;
-//using System;
-//using Vosk;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
 
 public class Text : MonoBehaviour
 {
@@ -11,13 +12,16 @@ public class Text : MonoBehaviour
     private float time = 5f;
 
     private Thread thread;
+    private Process process;
 
     public string text = "";
 
     public class Json
     {
-        [Newtonsoft.Json.JsonProperty("text")]
-        public int content { get; set; }
+        [Newtonsoft.Json.JsonProperty("status")]
+        public string status { get; set; }
+        [Newtonsoft.Json.JsonProperty("translatedText")]
+        public string text { get; set; }
     }
 
     void Start()
@@ -32,19 +36,20 @@ public class Text : MonoBehaviour
     {
         textContent.text = text;
 
-        //time -= Time.deltaTime;
+        time -= Time.deltaTime;
 
-        //if (time < 0)
-        //{
-        //    textContent.text = "";
+        if (time < 0)
+        {
+            textContent.text = "";
+            text = "";
 
-        //    time = 5;
-        //}
+            time = 5;
+        }
     }
 
     private void OnApplicationQuit()
     {
-        thread.Abort();
+        process.Kill();
     }
 
     void GetInfo()
@@ -55,39 +60,94 @@ public class Text : MonoBehaviour
         start.Arguments = string.Format("E:\\vosk-api-master\\python\\example\\test_microphone.py -d 5"); // 
         start.UseShellExecute = false;
         start.RedirectStandardOutput = true;
-        //start.WindowStyle = ProcessWindowStyle.Hidden;
+        start.WindowStyle = ProcessWindowStyle.Hidden;
+        start.StandardOutputEncoding = System.Text.Encoding.UTF8;
         UnityEngine.Debug.Log("1");
-        using (Process process = Process.Start(start))
+
+        process = new Process();
+        process.StartInfo = start;
+
+        process.OutputDataReceived += new DataReceivedEventHandler((sender, i) =>
         {
-            UnityEngine.Debug.Log("2");
-            using (System.IO.StreamReader reader = process.StandardOutput)
+            if (!System.String.IsNullOrEmpty(i.Data))
             {
-                UnityEngine.Debug.Log("3");
-                while (!reader.EndOfStream)
+                if(i.Data.Contains("text"))
                 {
-                    UnityEngine.Debug.Log("4");
-                    string result = reader.ReadLine();
+                    Json content = new Json();
 
                     try
                     {
-                        if(result.Contains("text"))
+
+                        string data = i.Data.Split('"')[3];
+                        //data = data.Substring(1, data.LastIndexOf('"')-1);
+
+                        //int x = 0;
+                        //foreach (string str in data)
+                        //{
+                        //    x++;
+                        //    UnityEngine.Debug.Log("nr: " + x + ", " + str);
+                        //}
+
+                        UnityEngine.Debug.Log(SwitcherToPolishCharacters("{ " + data + " }"));
+                        if (data != "")
                         {
-                            UnityEngine.Debug.Log("Result:" + result);
-                        } else
-                        {
-                            continue;
+                            data = Get(@"https://script.google.com/macros/s/AKfycbyXf4lSBuiJqXHdiZp6mz3NxEorEFY-EHoyDTY3CEEyuZb4vhVHQcHG9brbgCrrwMhT/exec?text=" + SwitcherToPolishCharacters(data).ToString());
+                            Json js = new Json();
+                            js = JsonConvert.DeserializeObject<Json>(data);
+                            if(js.status == "success")
+                            {
+                                text = js.text;
+                                time = 5f;
+                            }
                         }
-                    }
-                    catch(System.Exception e)
+                    } catch(System.Exception e)
                     {
-                        //UnityEngine.Debug.Log(e);
+                        UnityEngine.Debug.Log(e);
                     }
                 }
-                UnityEngine.Debug.Log("5");
-
             }
-            process.Kill();
-            thread.Abort();
+        });
+
+        process.Start();
+        process.BeginOutputReadLine();
+        process.WaitForExit();
+        process.Close();
+    }
+
+    private string SwitcherToPolishCharacters(string text)
+    {
+        System.Collections.Generic.Dictionary<string, string> letter = new System.Collections.Generic.Dictionary<string, string>()
+        {
+            {@"\xc4\x85", "¹"},
+            {@"\xc4\x87", "æ"},
+            {@"\xc4\x99", "ê"},
+            {@"\xc5\x82", "³"},
+            {@"\xc5\x84", "ñ"},
+            {@"\xc3\xb3", "ó"},
+            {@"\xc5\x9b", "œ"},
+            {@"\xc5\xbc", "¿"},
+            {@"\xc5\xba", "Ÿ"},
+        };
+
+        foreach(string letterKey in letter.Keys)
+        {
+            text = text.Replace(letterKey, letter[letterKey]);
+        }
+
+        return text;
+
+    }
+
+    public string Get(string uri)
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+        request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+        using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+        using (Stream stream = response.GetResponseStream())
+        using (StreamReader reader = new StreamReader(stream))
+        {
+            return reader.ReadToEnd();
         }
     }
 }
